99c75caec89a51d465366fbbed7771fbea72c45a
Arkadij Chistyj <arkdchst@gmail.com>
2020-05-03 16:56:01 +0000

    ListSequence implemented

--- main.cpp
+++ main.cpp
@@ -282,13 +282,13 @@ public:
 template <typename T> class Sequence{
 public:
 	int size = 0;
-public:
 	Sequence(){}
+public:
 
 	virtual T getFirst() const = 0;
 	virtual T getLast() const = 0;
 	virtual T get(int index) const = 0;
-	virtual int getSize() const { return size; }
+	virtual int getSize() const { return this->size; }
 
 
 	virtual Sequence<T>* getSubsequence(int start, int end) const = 0;
@@ -308,18 +308,23 @@ protected:
 	DynamicArray<T> *array;
 
 public:
-	ArraySequence() : Sequence<T>() {
+	ArraySequence(){
 		this->array = new DynamicArray<T>();
 		this->size = 0;
 	}
 
-	ArraySequence(int size) : Sequence<T>() {
-		this->array = new DynamicArray<T>(size);
-		this->size = size;
+	ArraySequence(const ArraySequence<T> &seq){
+		this->array = new DynamicArray<T>(seq.array);
+		this->size = seq.size;
+	}
+
+	ArraySequence(DynamicArray<T>* array){
+		this->array = array;
+		this->size = array->getSize();
 	}
 
 
-	ArraySequence(T *items, int size) : Sequence<T>() {
+	ArraySequence(T *items, int size){
 		this->array = new DynamicArray<T>(items, size);
 		this->size = size;
 	}
@@ -399,9 +404,10 @@ public:
 	}
 
 	virtual ArraySequence<T>* concat(const Sequence<T>& seq) const override {
-		ArraySequence<T> *newSequence = new ArraySequence<T>(this->size + seq.size);
+		DynamicArray<T> *array = new DynamicArray<T>(this->size + seq.size);
+		ArraySequence<T> *newSequence = new ArraySequence<T>(array);
 		for(int i = 0; i < this->size; i++)
-			newSequence->set(get(i), i);
+			newSequence->set(this->get(i), i);
 
 		for(int i = 0; i < seq.size; i++)
 			newSequence->set(seq.get(i), i + this->size);
@@ -411,6 +417,87 @@ public:
 
 };
 
+
+template <typename T> class ListSequence : public Sequence<T>{
+	LinkedList<T> *list;
+public:
+	ListSequence(){
+		this->list = new LinkedList<T>();
+		this->size = 0;
+	}
+
+	ListSequence(const ListSequence<T> &seq){
+		this->list = new LinkedList<T>(seq.list);
+		this->size = seq.size;
+	}
+
+	ListSequence(LinkedList<T> *list){
+		this->list = list;
+		this->size = list->getSize();
+	}
+
+	ListSequence(T *items, int size){
+		this->list = new LinkedList<T>(items, size);
+		this->size = size;
+	}
+
+	virtual ~ListSequence(){
+		delete this->list;
+	}
+
+
+	virtual T getFirst() const override {
+		return this->list->getFirst();
+	}
+	
+	virtual T getLast() const override {
+		return this->list->getLast();
+	}
+
+	virtual T get(int index) const override {
+		return this->list->get(index);
+	}
+
+
+	virtual Sequence<T>* getSubsequence(int start, int end) const override {
+		LinkedList<T> *subList = this->list->getSublist(start, end);
+		ListSequence<T> *seq = new ListSequence<T>(subList);
+		return seq;
+	}
+
+	virtual void set(const T &item, int index) override {
+		this->list->set(item, index);
+	}
+
+	virtual void append(const T &item) override {
+		this->list->append(item);
+		this->size++;
+	}
+
+	virtual void prepend(const T &item) override {
+		this->list->prepend(item);
+		this->size++;
+	}
+
+	virtual void insertAt(const T &item, int index) override {
+		this->list->insertAt(item, index);
+		this->size++;
+	}
+
+	virtual Sequence<T>* concat(const Sequence<T>& seq) const override {
+		ListSequence<T> *newSequence = new ListSequence<T>();
+
+		for(int i = 0; i < this->size; i++)
+			newSequence->append(this->get(i));
+
+		for(int i = 0; i < seq.size; i++)
+			newSequence->append(seq.get(i));
+
+		return newSequence;
+	}
+
+
+};
 
 
 int main(int argc, const char *argv[]){
