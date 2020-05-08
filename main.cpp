fc66405169705c003ff693fe4758b29851449adf
Arkadij Chistyj <arkdchst@gmail.com>
2020-05-08 20:15:40 +0000

    Matrix and DiagonalMatrix implemented

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <cstring>
 #include <cassert>
+#include <cmath>
 
 #include <stdexcept>
 
@@ -120,6 +121,20 @@ public:
 		for(int i = 0; i < size; i++){
 			*ptr = new Record;
 			(*ptr)->item = items[i];
+			ptr = &((*ptr)->next);
+		}
+
+		this->size = size;
+	}
+
+
+	LinkedList(int size) : LinkedList() {
+		if(size < 0) throw std::length_error(NEGATIVE_SIZE_MESSAGE);
+
+		Record **ptr = &(this->head);
+		for(int i = 0; i < size; i++){
+			*ptr = new Record;
+			(*ptr)->item = T();
 			ptr = &((*ptr)->next);
 		}
 
@@ -379,6 +394,11 @@ public:
 		this->size = size;
 	}
 
+	ArraySequence(int size){
+		this->array = new DynamicArray<T>(size);
+		this->size = size;
+	}
+
 	virtual ~ArraySequence(){
 		delete this->array;
 		this->size = 0;
@@ -493,6 +513,11 @@ public:
 		this->size = size;
 	}
 
+	ListSequence(int size){
+		this->list = new LinkedList<T>(size);
+		this->size = size;
+	}
+
 	virtual ~ListSequence(){
 		delete this->list;
 		this->size = 0;
@@ -549,5 +574,137 @@ public:
 		return newSequence;
 	}
 
+
+};
+
+
+template <typename T>
+class Matrix{
+protected:
+	int width = 0, height = 0;
+public:
+	virtual T get(int x, int y) const = 0;
+	virtual void set(T item, int x, int y) = 0;
+
+	int getWidth() const {
+		return this->width;
+	}
+
+	int getHeight() const {
+		return this->height;
+	}
+
+	bool operator==(const Matrix<T> &matrix) const {
+		if(this->width != matrix.width || this->height != matrix.height) return false;
+
+		for(int i = 0; i < this->width; i++)
+			for(int j = 0; j < this->height; j++)
+				if(this->get(i, j) != matrix.get(i, j)) return false;
+	
+
+		return true;
+	}
+};
+
+
+template <typename T>
+class DiagonalMatrix : public Matrix<T>{
+public:
+	Sequence<Sequence<T>*> *data;
+	int diag = 0; //diagonality
+
+	int getDiagLength(int x, int y) const {
+		return getDiagLength(getDiagNumber(x,y));
+	}
+
+	int getDiagLength(int diagNum) const {
+		diagNum = this->diag / 2 - diagNum;
+		int x = 0, y = 0;
+		if(diagNum >= 0){
+			x = diagNum;
+			y = 0;
+		}else{
+			x = 0;
+			y = -diagNum;
+		}
+
+		int t1 = this->width - x;
+		int t2 = this->height - y;
+		return (t1 < t2 ? t1 : t2);
+	}
+
+	int getDiagNumber(int x, int y) const {
+		return y - x + this->diag/2;
+	}
+
+public:
+
+	DiagonalMatrix(int width, int height, int diag){
+		if(width < 0 || height < 0 || diag < 0) throw std::length_error(NEGATIVE_SIZE_MESSAGE);
+
+		this->width = width;
+		this->height = height;
+		this->diag = diag;
+
+		data = (Sequence<Sequence<T>*>*) new ListSequence<ListSequence<T>*>;
+		for(int i = 0; i < diag; i++){
+			data->append(new ListSequence<T>(getDiagLength(i)));
+		}
+	}
+
+	T get(int x, int y) const override {
+		if(x < 0 || x >= this->width || y < 0 || y >= this->height) throw std::out_of_range(INDEX_OUT_OF_RANGE_MESSAGE);
+
+		int num = this->getDiagNumber(x, y);
+		if(num < 0 || num >= this->diag) return 0;
+
+		return data->get(getDiagNumber(x,y))->get( (x < y) ? x : y );
+	}
+
+	void set(T item, int x, int y) override {
+		if(x < 0 || x >= this->width || y < 0 || y >= this->height)  throw std::out_of_range(INDEX_OUT_OF_RANGE_MESSAGE);
+
+		int num = this->getDiagNumber(x, y);
+		if(num < 0 || num >= this->diag){
+			if(item != 0)
+				throw std::out_of_range("trying to set non-diagonal element");
+			return;
+		}
+
+		data->get(getDiagNumber(x,y))->set(item, (x < y) ? x : y );
+	}
+
+	DiagonalMatrix<T>* operator+(const DiagonalMatrix<T> &matrix) const {
+		DiagonalMatrix<T> *newMatrix = new DiagonalMatrix<T>(this->width, this->height, (this->diag > matrix.diag ? this->diag : matrix.diag));
+		for(int i = 0; i < this->width; i++)
+			for(int j = 0; j < this->height; j++){
+				newMatrix->set(this->get(i, j) + matrix.get(i, j), i, j);
+			}
+
+		return newMatrix;
+	}
+
+	template <typename U>
+	DiagonalMatrix<T>* operator*(U k) const {
+		DiagonalMatrix<T> *newMatrix = new DiagonalMatrix<T>(this->width, this->height, this->diag);
+		for(int i = 0; i < this->width; i++)
+			for(int j = 0; j < this->height; j++){
+				newMatrix->set(this->get(i, j) * k, i, j);
+			}
+
+		return newMatrix;
+	}
+
+
+	float getNorm() const {
+		float sum = 0;
+		for(int i = 0; i < this->width; i++)
+			for(int j = 0; j < this->height; j++){
+				T elem = this->get(i, j);
+				sum += elem * elem;
+			}
+
+		return sqrt(sum);
+	}
 
 };
